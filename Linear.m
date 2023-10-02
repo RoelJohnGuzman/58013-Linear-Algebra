734c5a1616bec1ba3a44717ca021c8f6d8d9382b
RoelJohnGuzman <123426505+RoelJohnGuzman@users.noreply.github.com>
2023-10-02 09:28:58 +0800

    Add files via upload

--- Linear.m
+++ Linear.m
@@ -21,3 +21,13 @@ disp(deter)
 disp('x is');
 ans = [x1, x2, x3];
 disp(ans)
+
+syms x y z;
+eq1 = x+y+z == 5;
+eq2 = x+2*y+2*z == 6;
+eq3 = x+2*y+3*z == 8;
+sol = solve([eq1,eq2,eq3],[x,y,z]);
+xsol = sol.x
+ysol = sol.y
+zsol = sol.z
+
