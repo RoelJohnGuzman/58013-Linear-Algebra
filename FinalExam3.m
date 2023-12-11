02f7b9ec285575fc36c000aad6b99e30eb6fdebf
RoelJohnGuzman <123426505+RoelJohnGuzman@users.noreply.github.com>
2023-12-11 08:38:28 +0800

    Add files via upload

---
+++ FinalExam3.m
@@ -0,0 +1,9 @@
+A = [1 1 1; 1 0 -1; 0 -3 1];
+B = [89; -6; 0];
+
+C = linsolve(A, B);
+disp(C)
+
+disp('Student A has 34.7143 Pesos')
+disp('Student B has 13.5714 Pesos')
+disp('Student C has 40.7143 pesos')
